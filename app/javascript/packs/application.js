7a45260d4fc5a3cd9496f1869f7671adfb505690
michowler <mlhy9778@hotmail.com>
2020-06-27 16:40:23 +0800

    react rendering

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require('bootstrap')
-require('components')
-import 'bootstrap/dist/css/bootstrap'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -19,4 +16,13 @@ import 'bootstrap/dist/css/bootstrap'
 // const imagePath = (name) => images(name, true)
 
 // Support component names relative to this directory:
+require('bootstrap')
+require('components')
+import 'bootstrap/dist/css/bootstrap'
 
+
+
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,5 @@
 class Job < ApplicationRecord
+	belongs_to :user
+	default_scope { order(created_at: :desc) }
+	validates :pickup_address, :dropoff_address, :pickup_lat, :pickup_long, :dropoff_lat, :dropoff_long, presence: true
 end
